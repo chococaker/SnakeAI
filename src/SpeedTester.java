025235331e13666f254e7d4a9510c5cfc222ee44
ultraeon <robloxrulesmariorules@gmail.com>
2025-02-20 21:45:00 -0600

    Outline for speed tester class

---
+++ src/SpeedTester.java
@@ -0,0 +1,8 @@
+public class SpeedTester {
+  public SpeedTester() {}
+  public int deepClone() {}
+  public int doTickS() {}
+  public int doTickE() {}
+  public int getBestMoveS() {}
+  public int getBestMoveE() {}
+}
