93670ac2e39d53a75996b05aa50bc7952c1f4325
ultraeon <robloxrulesmariorules@gmail.com>
2025-02-21 13:37:48 -0600

    Deep clone start done

--- src/SpeedTester.java
+++ src/SpeedTester.java
@@ -1,8 +1,33 @@
 public class SpeedTester {
   public SpeedTester() {}
-  public int deepClone() {}
-  public int doTickS() {}
-  public int doTickE() {}
-  public int getBestMoveS() {}
-  public int getBestMoveE() {}
+  
+  public long deepCloneS() {
+      long startTime = System.nanoTime();
+      SnakeBoard board = new SnakeBoard();
+      SnakeBoard temp;
+      for(int i = 0; i < 100000; i++) {
+          temp = board.deepClone();
+      }
+      return System.nanoTime()-startTime;
+  }
+  public long deepCloneE() {
+      SnakeBoard board = new SnakeBoard();
+      Node lengthAdvancer = new Node(board);
+      while(board.score < 80) {
+          if(!board.doTick(lengthAdvancer.iterDSolve(10), false)) {
+              System.out.println(board.score);
+              board = new SnakeBoard();
+          }
+      }
+      long startTime = System.nanoTime();
+      SnakeBoard temp;
+      for(int i = 0; i < 100000; i++) {
+          temp = board.deepClone();
+      }
+      return System.nanoTime()-startTime;
+  }
+  //public long doTickS() {}
+  //public long doTickE() {}
+  //public long getBestMoveS() {}
+  //public long getBestMoveE() {}
 }
